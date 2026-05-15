d5bec79444a7801037e993b8728067c10229c9cd
Yurii Dumansky <mojnolu4she2017@gmail.com>
2026-05-15 20:21:12 +0300

    added constants

---
+++ src/test/java/utils/constants/CommonConstants.java
@@ -0,0 +1,10 @@
+package utils.constants;
+
+import com.github.javafaker.Faker;
+
+public class CommonConstants {
+
+    public static final Faker faker = Faker.instance();
+
+    public static final String EMPTY_VALUE = "";
+}
